97bc38609f5fb146da10b31be4c14d6c4597966f
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-02 15:53:13 -0400

    Creación de UsuarioRepository

--- src/main/java/com/example/LoginAuth/repository/UsuarioRepository.java
+++ src/main/java/com/example/LoginAuth/repository/UsuarioRepository.java
@@ -1,5 +1,16 @@
 package com.example.LoginAuth.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.LoginAuth.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existByRut(String rut);
+
+    boolean existsByUsername(String username);
 }
