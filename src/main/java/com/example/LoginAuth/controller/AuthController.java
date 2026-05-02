65b285a05c4f813af3752f6615288f3daa765f05
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-02 16:50:28 -0400

    Creacion AuthController

---
+++ src/main/java/com/example/LoginAuth/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.LoginAuth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LoginAuth.dto.LoginRequestDTO;
+import com.example.LoginAuth.dto.UsuarioResponseDTO;
+import com.example.LoginAuth.service.AuthService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+    private AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioResponseDTO> login(@Valid @RequestBody LoginRequestDTO request){
+
+        UsuarioResponseDTO response = authService.login(request);
+
+        return ResponseEntity.ok(response);
+
+    }
+
+
+
+}
