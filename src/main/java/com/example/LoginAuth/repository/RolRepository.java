d97aa1df8ee50cd8eed317580483b299e8ea5ca0
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-02 15:46:03 -0400

    Creacion RolRepository

---
+++ src/main/java/com/example/LoginAuth/repository/RolRepository.java
@@ -0,0 +1,21 @@
+package com.example.LoginAuth.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.LoginAuth.model.Rol;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Long>{
+    // extends JpaRepo obtiene los 10 metodos de bd .saveAll findAll etc
+    // el <Rol, Long> son dos instrucciones precisas para que funcione, la tabla Rol y busque el tipo de dato Long que es la llave primaria
+
+
+
+    //Optional ayuda que al buscar un rol que no exista, devuelva un null en vez de un error(nullpointerexception)
+    Optional<Rol> findByNombreRol(String nombreRol);
+
+}
---
+++ src/main/java/com/example/LoginAuth/repository/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package com.example.LoginAuth.repository;
+
+public class UsuarioRepository {
+    
+}
