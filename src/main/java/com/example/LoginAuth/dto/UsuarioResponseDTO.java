78a9a7d670b9c7ab77c65d9d5056f645b10a20a7
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-02 16:01:23 -0400

    Creacion de UsuarioDTO

---
+++ src/main/java/com/example/LoginAuth/dto/UsuarioResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.LoginAuth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioResponseDTO {
+    
+    private Long id;
+    private String nombre;
+    private String rut;
+    private String username;
+    private String nombreRol;
+}
