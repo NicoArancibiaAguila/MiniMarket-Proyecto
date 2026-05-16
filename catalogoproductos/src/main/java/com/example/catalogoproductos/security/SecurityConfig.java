27b4b03196e7c18e348fd4585653d48702125b3e
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-16 18:17:21 -0400

    Seguridad JWT creada

---
+++ catalogoproductos/src/main/java/com/example/catalogoproductos/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.catalogoproductos.security;
+
+import com.example.catalogoproductos.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor // Esto inyectará el filtro automáticamente
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                // acceso libre para ver productos
+                .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
+                // proteccion a la creación y eliminación
+                .requestMatchers(HttpMethod.POST, "/api/productos/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/api/productos/**").authenticated()
+                .anyRequest().authenticated()
+            );
+
+        // aca spring pasa peticiones por el filtro JWT antes del filtro de autenticacion estandar
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
