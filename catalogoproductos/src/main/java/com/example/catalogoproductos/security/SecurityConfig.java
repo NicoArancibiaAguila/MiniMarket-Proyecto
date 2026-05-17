30d8977ca5cd5680001ce3df8944511554102f86
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-17 16:33:15 -0400

    cambio a securityConfig para que solo admin tenga permiso de delete-post

--- catalogoproductos/src/main/java/com/example/catalogoproductos/security/SecurityConfig.java
+++ catalogoproductos/src/main/java/com/example/catalogoproductos/security/SecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -28,9 +26,10 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 // acceso libre para ver productos
                 .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
-                // proteccion a la creación y eliminación
-                .requestMatchers(HttpMethod.POST, "/api/productos/**").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/api/productos/**").authenticated()
+                // proteccion, solo ADMIN puede eliminar o crear
+                .requestMatchers(HttpMethod.POST, "/api/productos/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/productos/**").hasRole("ADMIN")
+                //aca cualquier otra ruta, pide un token basico 
                 .anyRequest().authenticated()
             );
 
@@ -40,8 +39,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
