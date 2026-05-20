c7cad69c09a139013672afd6ee5a79e8a210f29e
NicoArancibiaAguila <nicola.arancibiaa@duocuc.cl>
2026-05-20 11:53:07 -0400

    feat: cambio pequeño a InventarioCliente

--- catalogoproductos/src/main/java/com/example/catalogoproductos/client/InventarioClient.java
+++ catalogoproductos/src/main/java/com/example/catalogoproductos/client/InventarioClient.java
@@ -1,17 +1,19 @@
 package com.example.catalogoproductos.client;
 
 import com.example.catalogoproductos.dto.ProductoRequestDTO;
+import com.example.catalogoproductos.security.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "inventario-service",
-        url = "http://localhost:8084/"
+        url = "http://localhost:8084/",
+        configuration = FeignConfig.class
 )
 public interface InventarioClient {
 
- @PostMapping("/api/inventario/crear")
+@PostMapping("/api/inventario/crear")
 Object crearInventario(
         @RequestBody ProductoRequestDTO dto
 );
