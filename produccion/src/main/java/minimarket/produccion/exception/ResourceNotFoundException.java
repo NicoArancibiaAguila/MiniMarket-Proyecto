f6b660e22d062ba6e2e751c190362f92fe3b928d
Ben0zero <be.gonzalezy@duocuc.cl>
2026-06-23 22:24:16 -0400

    Delete produccion directory

--- produccion/src/main/java/minimarket/produccion/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package minimarket.produccion.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
-
